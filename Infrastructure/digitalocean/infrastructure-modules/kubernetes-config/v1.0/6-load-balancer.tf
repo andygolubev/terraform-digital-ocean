822cda0566637299b17e13b217f051ccca0af8a9
Andy Golubev <verkomf@gmail.com>
2023-05-17 11:05:22 +0300

    fix dependency

--- Infrastructure/digitalocean/infrastructure-modules/kubernetes-config/v1.0/6-load-balancer.tf
+++ Infrastructure/digitalocean/infrastructure-modules/kubernetes-config/v1.0/6-load-balancer.tf
@@ -2,21 +2,22 @@
 # It uses doctl (digital ocean cli tool), so doctl must be authorized null_resource.kubeconfig_save resource
 
 resource "local_file" "get_load_balancer_script" {
-    content  = <<-EOF
-    #!/bin/bash
-    doctl kubernetes cluster list-associated-resources $1 -o json | jq '{ load_balancer_id: .load_balancers[0].id, load_balancer_name: .load_balancers[0].name }'
-    EOF
+  content  = <<-EOF
+  #!/bin/bash
+  doctl kubernetes cluster list-associated-resources $1 -o json | jq '{ load_balancer_id: .load_balancers[0].id, load_balancer_name: .load_balancers[0].name }'
+  EOF
 
-    filename = "/tmp/get_load_balancer_id.sh"
+  filename = "/tmp/get_load_balancer_id.sh"
 
-    depends_on = [ kubernetes_ingress_v1.demo-ingress, ]
+  depends_on = [ kubernetes_ingress_v1.demo-ingress, kubernetes_service_v1.ingress-nginx-controller, ]
 }
 
 data "external" "load_balancer_details" {
-    program = ["${local_file.get_load_balancer_script.filename}", "${var.k8s_config_cluster_name}"]
-    depends_on = [ local_file.get_load_balancer_script, ]
+  program = ["${local_file.get_load_balancer_script.filename}", "${var.k8s_config_cluster_name}"]
+  depends_on = [ local_file.get_load_balancer_script, ]
 }
 
 data "digitalocean_loadbalancer" "this" {
   id = data.external.load_balancer_details.result.load_balancer_id
+  depends_on = [ local_file.get_load_balancer_script, ]
 }
